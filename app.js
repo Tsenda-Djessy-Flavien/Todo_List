bb0338bb3bd662c2ccdfec15fd3ce1b61f431fb6
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-08 17:24:28 +0100

    condition pour ne pas ajouter une list vide

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
 
 function addGoal() {
-    // recuperer la value saisi
-    const enteredValue = inputEl.value;
-    // ensuite creer un list d'element 
-    const listItemEl = document.createElement('li');
-    // inject la value saisi dans la list d'element 
-    listItemEl.textContent = enteredValue;
-    // ajouter la list d'element dans la list principale
-    listEl.appendChild(listItemEl);
-    // reinitialiser la list après avoir ajoute la value
-    inputEl.value = '';
+    if (inputEl.value != '') {
+        // recuperer la value saisi
+        const enteredValue = inputEl.value;
+        // ensuite creer un list d'element 
+        const listItemEl = document.createElement('li');
+        // inject la value saisi dans la list d'element 
+        listItemEl.textContent = enteredValue;
+        // ajouter la list d'element dans la list principale
+        listEl.appendChild(listItemEl);
+        // reinitialiser la list après avoir ajoute la value
+        inputEl.value = '';
+    }
 }
 
 buttonEl.addEventListener('click', addGoal);
